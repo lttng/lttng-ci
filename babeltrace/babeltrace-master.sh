aad6ac9072f76adb34d053ba5319e53067ea94c7
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2015-01-27 17:57:52 -0500

    Add build tree configuration for babeltrace

--- babeltrace/babeltrace-master.sh
+++ babeltrace/babeltrace-master.sh
@@ -26,18 +26,57 @@ python_bindings)
     ;;
 esac
 
-./configure --prefix=$PREFIX $CONF_OPTS
+# Build type
+# oot : out-of-tree build
+# dist: build via make dist
+# *   : normal tree build
+#
+# Make sure to move to the build_path and configure
+# before continuing
 
-make
+BUILD_PATH=$WORKSPACE
+case "$build" in
+	oot)
+		echo "Out of tree build"
+		BUILD_PATH=$WORKSPACE/oot
+		mkdir -p $BUILD_PATH
+		cd $BUILD_PATH
+		$WORKSPACE/configure --prefix=$PREFIX $CONF_OPTS
+		;;
+	dist)
+		echo "Distribution out of tree build"
+		BUILD_PATH=`mktemp -d`
+
+		# Initial configure and generate tarball
+		./configure
+		make dist
+
+		mkdir -p $BUILD_PATH
+		cp *.tar.* $BUILD_PATH/
+		cd $BUILD_PATH
+
+		# Ignore level 1 of tar
+		tar xvf *.tar.* --strip 1
+
+		$BUILD_PATH/configure --prefix=$PREFIX $CONF_OPTS
+		;;
+	*)
+		BUILD_PATH=$WORKSPACE
+		echo "Standard tree build"
+		$WORKSPACE/configure --prefix=$PREFIX $CONF_OPTS
+		;;
+esac
+
+make V=1
 make install
 
 rm -rf $WORKSPACE/tap
 mkdir -p $WORKSPACE/tap
 
-cd $WORKSPACE/tests
+cd $BUILD_PATH/tests
 
 # Run make check tests
-prove --merge --exec '' - < $WORKSPACE/tests/tests --archive $WORKSPACE/tap/ || true
+prove --merge --exec '' - < $BUILD_PATH/tests/tests --archive $WORKSPACE/tap/ || true
 
 # TAP plugin is having a hard time with .yml files.
 rm -f $WORKSPACE/tap/meta.yml
@@ -51,3 +90,7 @@ make clean
 find $WORKSPACE/build/bin -executable -type f -exec chrpath --delete {} \;
 find $WORKSPACE/build/lib -name "*.so" -exec chrpath --delete {} \;
 find $WORKSPACE/build/lib -name "*.la" -exec rm -f {} \;
+
+if [ $build = "dist" ]; then
+	rm -rf $BUILD_PATH
+fi
