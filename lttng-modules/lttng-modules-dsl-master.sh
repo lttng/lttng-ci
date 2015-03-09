b1a784835cdd17b4bf6a8bfbfaf2efb4e95731bf
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2015-03-09 12:41:52 -0400

    Clean on start and move to git modules subdirectory

--- lttng-modules/lttng-modules-dsl-master.sh
+++ lttng-modules/lttng-modules-dsl-master.sh
@@ -1,8 +1,13 @@
 # Recreate link to kernel source
+
+rm -rf /tmp/linux-source
+rm -rf /tmp/linux-artifact
+
 ln -s $WORKSPACE/linux-source /tmp/linux-source
 ln -s $WORKSPACE/linux-artifact /tmp/linux-artifact
 ln -s /tmp/linux-source /tmp/linux-artifact/source
 
+cd lttng-modules
 make KERNELDIR=/tmp/linux-artifact
 #make INSTALL_MOD_PATH="$PREFIX" modules_install
 rm -rf /tmp/linux-source
