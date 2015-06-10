badc51eb5bbd0dbbd75aa6ac07e1553049d66609
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2015-06-10 14:55:23 -0400

    demo

--- dsl/kernel-lttng-modules.seed.groovy
+++ dsl/kernel-lttng-modules.seed.groovy
@@ -87,7 +87,7 @@ class BasicVersion implements Comparable<BasicVersion> {
     }
 }
 
-def kernelTagCutOff = new BasicVersion("4.0", "")
+def kernelTagCutOff = new BasicVersion("3.0", "")
 def modulesBranches = ["master","stable-2.5.0","stable-2.6.0", "stable-2.4.0"]
 
 
