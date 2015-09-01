2b68721ab3a069c3763abf5746a7a8823d804791
Michael Jeanson <mjeanson@efficios.com>
2015-09-01 13:41:31 -0400

    Initial lttng-ust jobs

--- scripts/lttng-ust/build.sh
+++ scripts/lttng-ust/build.sh
@@ -1,14 +1,32 @@
+#!/bin/sh -exu
+#
+# Copyright (C) 2015 - Jonathan Rajotte-Julien <jonathan.rajotte-julien@efficios.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # Create build directory
 rm -rf $WORKSPACE/build
 mkdir -p $WORKSPACE/build
 
 # liburcu
-URCU_INCS="$WORKSPACE/dependencies/liburcu/build/include/"
-URCU_LIBS="$WORKSPACE/dependencies/liburcu/build/lib/"
+URCU_INCS="$WORKSPACE/deps/liburcu/build/include/"
+URCU_LIBS="$WORKSPACE/deps/liburcu/build/lib/"
 
 export CPPFLAGS="-I$URCU_INCS"
 export LDFLAGS="-L$URCU_LIBS"
-export LD_LIBRARY_PATH="$URCU_LIBS:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$URCU_LIBS:${LD_LIBRARY_PATH:-}"
 
 PREFIX="$WORKSPACE/build"
 
@@ -27,13 +45,57 @@ java-agent)
     export CLASSPATH="/usr/share/java/log4j-1.2.jar"
     CONF_OPTS="--enable-java-agent-all"
     ;;
+python-agent)
+	echo "Python agent build"
+	CONF_OPTS="--enable-python-agent"
+	;;
 *)
     echo "Standard build"
     CONF_OPTS=""
     ;;
 esac
 
-./configure --prefix=$PREFIX $CONF_OPTS
+# Build type
+# oot : out-of-tree build
+# dist: build via make dist
+# *   : normal tree build
+#
+# Make sure to move to the build_path and configure
+# before continuing
+
+BUILD_PATH=$WORKSPACE
+case "$build" in
+	oot)
+		echo "Out of tree build"
+		BUILD_PATH=$WORKSPACE/oot
+		mkdir -p $BUILD_PATH
+		cd $BUILD_PATH
+		$WORKSPACE/configure --prefix=$PREFIX $CONF_OPTS
+		;;
+	dist)
+		echo "Distribution out of tree build"
+		BUILD_PATH=`mktemp -d`
+
+		# Initial configure and generate tarball
+		./configure
+		make dist
+
+		mkdir -p $BUILD_PATH
+		cp *.tar.* $BUILD_PATH/
+		cd $BUILD_PATH
+
+		# Ignore level 1 of tar
+		tar xvf *.tar.* --strip 1
+
+		$BUILD_PATH/configure --prefix=$PREFIX $CONF_OPTS
+		;;
+	*)
+		BUILD_PATH=$WORKSPACE
+		echo "Standard tree build"
+		$WORKSPACE/configure --prefix=$PREFIX $CONF_OPTS
+		;;
+esac
+
 make V=1
 make install
 
@@ -41,9 +103,9 @@ make install
 rm -rf $WORKSPACE/tap
 mkdir -p $WORKSPACE/tap/unit
 
-cd $WORKSPACE/tests
+cd $BUILD_PATH/tests
 
-prove --merge --exec '' - < $WORKSPACE/tests/unit_tests --archive $WORKSPACE/tap/unit/ || true
+prove --merge --exec '' - < $BUILD_PATH/tests/unit_tests --archive $WORKSPACE/tap/unit/ || true
 
 # TAP plugin is having a hard time with .yml files.
 rm -f $WORKSPACE/tap/unit/meta.yml
@@ -57,3 +119,8 @@ make clean
 # Cleanup rpath and libtool .la files
 find $WORKSPACE/build/lib -name "*.so" -exec chrpath --delete {} \;
 find $WORKSPACE/build/lib -name "*.la" -exec rm -f {} \;
+
+# Clean temp dir for dist build
+if [ $build = "dist" ]; then
+	rm -rf $BUILD_PATH
+fi
---
+++ scripts/lttng-ust/scan-build.sh
@@ -0,0 +1,66 @@
+#!/bin/sh -exu
+#
+# Copyright (C) 2015 - Jonathan Rajotte-Julien <jonathan.rajotte-julien@efficios.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# do not exit immediately if any command fails
+set +e
+ 
+# temp directory to store the scan-build report
+SCAN_BUILD_TMPDIR=$( mktemp -d /tmp/scan-build.XXXXXX )
+ 
+# directory to use for archiving the scan-build report
+SCAN_BUILD_ARCHIVE="${WORKSPACE}/scan-build-archive"
+
+# Create build directory
+rm -rf $WORKSPACE/build
+mkdir -p $WORKSPACE/build
+
+export CFLAGS="-O0 -g -DDEBUG"
+PREFIX="$WORKSPACE/build"
+
+./bootstrap
+./configure --prefix=$PREFIX
+make clean
+# generate the scan-build report
+scan-build -k -o ${SCAN_BUILD_TMPDIR} make
+ 
+# get the directory name of the report created by scan-build
+SCAN_BUILD_REPORT=$( find ${SCAN_BUILD_TMPDIR} -maxdepth 1 -not -empty -not -name `basename ${SCAN_BUILD_TMPDIR}` )
+rc=$?
+ 
+if [ -z "${SCAN_BUILD_REPORT}" ]; then
+    echo ">>> No new bugs identified."
+    echo ">>> No scan-build report has been generated"
+else
+    echo ">>> New scan-build report generated in ${SCAN_BUILD_REPORT}"
+ 
+    if [ ! -d "${SCAN_BUILD_ARCHIVE}" ]; then
+        echo ">>> Creating scan-build archive directory"
+        install -d -o jenkins -g jenkins -m 0755 "${SCAN_BUILD_ARCHIVE}"
+    else
+        echo ">>> Removing any previous scan-build reports from ${SCAN_BUILD_ARCHIVE}"
+        rm -f ${SCAN_BUILD_ARCHIVE}/*
+    fi
+ 
+    echo ">>> Archiving scan-build report to ${SCAN_BUILD_ARCHIVE}"
+    mv ${SCAN_BUILD_REPORT}/* ${SCAN_BUILD_ARCHIVE}/
+ 
+    echo ">>> Removing any temporary files and directories"
+    rm -rf "${SCAN_BUILD_TMPDIR}"
+fi
+ 
+exit ${rc}
