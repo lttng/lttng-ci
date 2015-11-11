72f4f0c1881d89d5dc8e1a7887500cc6b365aace
Michael Jeanson <mjeanson@efficios.com>
2015-11-11 15:36:17 -0500

    Add arm64 to liburcu and normalize build script

--- scripts/liburcu/build.sh
+++ scripts/liburcu/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -exu
+#!/bin/bash -exu
 #
 # Copyright (C) 2015 - Jonathan Rajotte-Julien <jonathan.rajotte-julien@efficios.com>
 #
@@ -22,10 +22,7 @@ mkdir -p $WORKSPACE/build
 
 PREFIX="$WORKSPACE/build"
 
-./bootstrap
-
-CONF_OPTS=""
-
+# Set platform variables
 case "$arch" in
 solaris10)
     MAKE=gmake
@@ -33,6 +30,7 @@ solaris10)
     NPROC=gnproc
     CFLAGS="-D_XOPEN_SOURCE=1 -D_XOPEN_SOURCE_EXTENDED=1 -D__EXTENSIONS__=1"
     ;;
+
 solaris11)
     MAKE=gmake
     TAR=gtar
@@ -40,6 +38,7 @@ solaris11)
     CFLAGS="-D_XOPEN_SOURCE=1 -D_XOPEN_SOURCE_EXTENDED=1 -D__EXTENSIONS__=1"
     export PATH="$PATH:/usr/perl5/bin"
     ;;
+
 *)
     MAKE=make
     TAR=tar
@@ -48,20 +47,29 @@ solaris11)
     ;;
 esac
 
+# Set configure options for each build configuration
+CONF_OPTS=""
 case "$conf" in
 static)
     echo "Static build"
     CONF_OPTS="--enable-static --disable-shared"
     ;;
+
 tls_fallback)  
     echo  "Using pthread_getspecific() to emulate TLS"
     CONF_OPTS="--disable-compiler-tls"
     ;;
+
 *)
     echo "Standard build"
     CONF_OPTS=""
     ;;
 esac
+
+
+# Run bootstrap prior to configure
+./bootstrap
+
 
 # Build type
 # oot : out-of-tree build
@@ -70,54 +78,64 @@ esac
 #
 # Make sure to move to the build_path and configure
 # before continuing
-
 BUILD_PATH=$WORKSPACE
 case "$build" in
-	oot)
-		echo "Out of tree build"
-		BUILD_PATH=$WORKSPACE/oot
-		mkdir -p $BUILD_PATH
-		cd $BUILD_PATH
-		MAKE=$MAKE CFLAGS="$CFLAGS" $WORKSPACE/configure --prefix=$PREFIX $CONF_OPTS
-		;;
-	dist)
-		echo "Distribution out of tree build"
-		BUILD_PATH=`mktemp -d`
+oot)
+    echo "Out of tree build"
+    BUILD_PATH=$WORKSPACE/oot
+    mkdir -p $BUILD_PATH
+    cd $BUILD_PATH
+    MAKE=$MAKE CFLAGS="$CFLAGS" $WORKSPACE/configure --prefix=$PREFIX $CONF_OPTS
+    ;;
 
-		# Initial configure and generate tarball
-		MAKE=$MAKE ./configure
-		$MAKE dist
+dist)
+    echo "Distribution out of tree build"
+    BUILD_PATH=`mktemp -d`
 
-		mkdir -p $BUILD_PATH
-		cp *.tar.* $BUILD_PATH/
-		cd $BUILD_PATH
+    # Initial configure and generate tarball
+    MAKE=$MAKE ./configure
+    $MAKE dist
 
-		# Ignore level 1 of tar
-		$TAR xvf *.tar.* --strip 1
+    mkdir -p $BUILD_PATH
+    cp *.tar.* $BUILD_PATH/
+    cd $BUILD_PATH
 
-		MAKE=$MAKE CFLAGS="$CFLAGS" $BUILD_PATH/configure --prefix=$PREFIX $CONF_OPTS
-		;;
-	*)
-		BUILD_PATH=$WORKSPACE
-		echo "Standard tree build"
-		MAKE=$MAKE CFLAGS="$CFLAGS" $WORKSPACE/configure --prefix=$PREFIX $CONF_OPTS
-		;;
+    # Ignore level 1 of tar
+    $TAR xvf *.tar.* --strip 1
+
+    MAKE=$MAKE CFLAGS="$CFLAGS" $BUILD_PATH/configure --prefix=$PREFIX $CONF_OPTS
+    ;;
+*)
+    BUILD_PATH=$WORKSPACE
+    echo "Standard tree build"
+    MAKE=$MAKE CFLAGS="$CFLAGS" $WORKSPACE/configure --prefix=$PREFIX $CONF_OPTS
+    ;;
 esac
 
+# BUILD!
 $MAKE -j `$NPROC`
 $MAKE install
+
+# Run tests
 $MAKE check
-$MAKE regtest
+#if [ "$version" >=" 0.9" ]; then
+   $MAKE regtest
+#fi
+
+# Cleanup
 $MAKE clean
 
-# Cleanup rpath and libtool .la files
+# Cleanup rpath in executables and shared libraries
+#find $WORKSPACE/build/bin -type f -perm -0500 -exec chrpath --delete {} \;
 find $WORKSPACE/build/lib -name "*.so" -exec chrpath --delete {} \;
+
+# Remove libtool .la files
 find $WORKSPACE/build/lib -name "*.la" -exec rm -f {} \;
 
 # Cleanup temp directory of dist build
-if [ $build = "dist" ]; then
-	cd $WORKSPACE
-	rm -rf $BUILD_PATH
+if [ "$build" = "dist" ]; then
+    cd $WORKSPACE
+    rm -rf $BUILD_PATH
 fi
 
 # EOF
