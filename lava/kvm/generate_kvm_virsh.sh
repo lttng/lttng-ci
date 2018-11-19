0bff60fb7ea5debc26fce47d6dcdd2f78512e6c3
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2018-11-19 16:10:26 -0500

    Use system libvirt session instead of user
    By default virsh use qemu:///session instead of qemu:///system.
    
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- lava/kvm/generate_kvm_virsh.sh
+++ lava/kvm/generate_kvm_virsh.sh
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+export LIBVIRT_DEFAULT_URI="qemu:///system"
+
 tmp=$(mktemp)
 virt-install --print-xml \
 	--name "$1" \
