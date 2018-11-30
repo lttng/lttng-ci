23ba1b76d4a709064677372d3002bf40e9f14594
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2018-11-29 21:46:26 -0500

    Deactivate lava 1 submission
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- scripts/system-tests/run-kvm-tests.sh
+++ scripts/system-tests/run-kvm-tests.sh
@@ -31,16 +31,6 @@ python -u "$LTTNG_CI_PATH"/scripts/system-tests/lava2-submit.py \
                           -lm "$S3_URL_LTTNG_MODULES" \
                           -tc "$LTTNG_TOOLS_COMMIT_ID" \
                           -uc "$LTTNG_UST_COMMIT_ID" \
-                          -id "$BUILD_TAG" \
-                          --debug
-
-python -u "$LTTNG_CI_PATH"/scripts/system-tests/lava-submit.py \
-                          -t kvm-tests \
-                          -j "$JOB_NAME" \
-                          -k "$STORAGE_KERNEL_IMAGE" \
-                          -lm "$STORAGE_LTTNG_MODULES" \
-                          -tc "$LTTNG_TOOLS_COMMIT_ID" \
-                          -uc "$LTTNG_UST_COMMIT_ID" \
                           -id "$BUILD_TAG"
 
 set +eu
