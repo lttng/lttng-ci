88b3026879d7a56fbb2c892b8a1c9bca0fecc3b4
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2018-12-18 15:36:41 -0500

    Revert "LAVA: use verbose mode for vlttng"
    This reverts commit d10d8fec412c0db01b4c8fef5264284d9ed80573.

--- scripts/system-tests/lava2-submit.py
+++ scripts/system-tests/lava2-submit.py
@@ -123,8 +123,7 @@ def get_vlttng_cmd(device, lttng_tools_commit, lttng_ust_commit=None):
                     ' --profile babeltrace-python' \
                     ' --profile lttng-tools-master' \
                     ' --override projects.lttng-tools.checkout='+lttng_tools_commit + \
-                    ' --profile lttng-tools-no-man-pages' \
-                    ' --verbose'
+                    ' --profile lttng-tools-no-man-pages'
 
     if lttng_ust_commit is not None:
         vlttng_cmd += ' --profile lttng-ust-master ' \
