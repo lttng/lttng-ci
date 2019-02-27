7fec8f14c244ea3d0013aef5e113230ba5409a7b
Michael Jeanson <mjeanson@efficios.com>
2019-02-27 18:05:06 -0500

    jjb: disable Ubuntu-hwe-4.18.0 branch for bionic builds
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>

--- scripts/lttng-modules/master.groovy
+++ scripts/lttng-modules/master.groovy
@@ -341,7 +341,7 @@ if (uversion != null) {
     case 'bionic':
       matchStrs = [
         ~/^refs\/tags\/(Ubuntu-4\.15\.0-\d{1,3}?\.[\d]+)$/,
-        ~/^refs\/tags\/(Ubuntu-hwe-4\.18\.0-.*_18\.04\.\d+)$/,
+//        ~/^refs\/tags\/(Ubuntu-hwe-4\.18\.0-.*_18\.04\.\d+)$/,
       ]
       break
 
