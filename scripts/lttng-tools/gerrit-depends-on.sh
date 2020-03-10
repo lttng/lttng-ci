3f7ea864199cffe7601489aa0b3a2aeccf9c0695
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2020-03-10 14:56:31 -0400

    Introduce Depends-on for lttng-tools gerrit job
    The development of a lttng-tools feature can happen in parallel with a
    lttng-ust feature. The need to build and test a lttng-tools gerrit
    change against a gerrit lttng-ust change arises.
    
    This patch introduce gerrit-based inter project dependency
    testing for the lttng-tools project against lttng-ust.
    
    The dependency can be expressed at the change's commit message level:
    
        Depends-on: lttng-ust: <lttng-ust-change-id>
    
    e.g
        Fix: client_flush_outgoing_queue expect an active client
    
        Move validated and active assignation before the call to
        client_flush_outgoing_queue.
    
        Depends-on: lttng-ust: If8557b77f2a03b05b924cdc26ecf7618fff46289
    
        Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
        Change-Id: I873b2fad567eb6d59a53a12cba02451afd1d9827
    
    For now, only lttng-ust is supported but babeltrace and lttng-modules
    could someday be prime candidate for "Depends-on".
    
    Note that the "Depends-on" line is optional. If none is present, we
    import lttng-ust from the lttng-ust base job as it is done today.
    
    For building ust, we reuse the script "scripts/lttng-ust/build.sh".
    
    Note that we override the "$build" variable to "std" for the dependency
    since the build type for the lttng-ust dependency is irrelevant for the
    current job.
    
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

---
+++ scripts/lttng-tools/gerrit-depends-on.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -exu
+# shellcheck disable=SC2103
+#
+# Copyright (C) 2020 Jonathan Rajotte-Julien <jonathan.rajotte-julien@efficios.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#Required variables
+GERRIT_NAME=${GERRIT_NAME:-}
+WORKSPACE=${WORKSPACE:-}
+conf=${conf:-}
+
+gerrit_url="https://${GERRIT_NAME}"
+gerrit_query="?o=CURRENT_REVISION&o=DOWNLOAD_COMMANDS"
+gerrit_json_query=".revisions[.current_revision].ref"
+
+possible_depends_on="lttng-ust"
+re="Depends-on: (${possible_depends_on}): ([^'$'\n'']*)"
+property_file="${WORKSPACE}/gerrit_custom_dependencies.properties"
+
+# Create the property file even if it ends up being empty
+touch "$property_file"
+
+# Move to lttng-tools source directory
+pushd "${WORKSPACE}/src/lttng-tools"
+
+git rev-list --format=%B --max-count=1 HEAD | while read -r line; do
+    if ! [[ ${line} =~ ${re} ]]; then
+        continue
+    fi
+
+    project=${BASH_REMATCH[1]}
+    gerrit_id=${BASH_REMATCH[2]}
+
+    project_sanitize=${BASH_REMATCH[1]//-/_}
+
+    if [ "$conf" = "no-ust" ] && [ "$project" = "lttng-ust" ]; then
+        # No need to checkout lttng-ust for this configuration axis
+        continue
+    fi
+
+    # Export the GERRIT_DEP_... into the property file for further jenkins usage
+    echo "GERRIT_DEP_${project_sanitize^^}=${gerrit_id}" >> "$property_file"
+
+    # Get the change latest ref
+    ref=$(curl "${gerrit_url}/changes/${gerrit_id}${gerrit_query}" | tail -n+2 | jq -r "$gerrit_json_query")
+    git clone "${gerrit_url}/${project}" "$WORKSPACE/src/$project"
+    pushd "$WORKSPACE/src/$project"
+    git fetch "${gerrit_url}/${project}" "$ref"
+    git checkout FETCH_HEAD
+    popd
+done
+
+popd
