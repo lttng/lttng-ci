942c30466ead5ae90452bc97d61c5d761b2ad16e
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2020-06-30 10:45:12 -0400

    system-tests: vm_test trigger kernel version
    2.10 is "unsupported".
    
    5.4 is "long term"
    
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- scripts/system-tests/system-trigger.groovy
+++ scripts/system-tests/system-trigger.groovy
@@ -302,9 +302,9 @@ final String pastJobsPath = build.getEnvironment(listener).get('WORKSPACE') + "/
 
 def recentLttngBranchesOfInterest = ['master',
   'stable-2.12',
-  'stable-2.11',
-  'stable-2.10']
+  'stable-2.11']
 def recentLinuxBranchesOfInterest = ['master',
+  'linux-5.4.y',
   'linux-5.1.y',
   'linux-5.0.y',
   'linux-4.19.y',
