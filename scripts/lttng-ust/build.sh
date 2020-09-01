5bd69da1c5de97265745de0a5aa9dbb4e1adeca0
Michael Jeanson <mjeanson@efficios.com>
2020-09-01 16:12:23 -0400

    jjb: lttng-ust: Add PKG_CONFIG_PATH env var
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>

--- scripts/lttng-ust/build.sh
+++ scripts/lttng-ust/build.sh
@@ -80,10 +80,12 @@ cc=${cc:-}
 
 DEPS_INC="$WORKSPACE/deps/build/include"
 DEPS_LIB="$WORKSPACE/deps/build/lib"
+DEPS_PKGCONFIG="$DEPS_LIB/pkgconfig"
 #DEPS_BIN="$WORKSPACE/deps/build/bin"
 #DEPS_JAVA="$WORKSPACE/deps/build/share/java"
 
 export LD_LIBRARY_PATH="$DEPS_LIB:${LD_LIBRARY_PATH:-}"
+export PKG_CONFIG_PATH="$DEPS_PKGCONFIG"
 export CPPFLAGS="-I$DEPS_INC"
 export LDFLAGS="-L$DEPS_LIB"
 
