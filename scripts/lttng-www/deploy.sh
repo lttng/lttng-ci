5592538db80a934f0cabb83dab25d8d35a6b3669
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2021-02-26 10:51:42 -0500

    Cleanup: whitespace
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- scripts/lttng-www/deploy.sh
+++ scripts/lttng-www/deploy.sh
@@ -27,7 +27,7 @@ cp "$KEY_FILE_VARIABLE" ~/.ssh/id_rsa
 # Nodejs
 # Using Debian, as root
 curl -fsSL https://deb.nodesource.com/setup_15.x | bash -
-apt-get install -y nodejs 
+apt-get install -y nodejs
 
 apt-get install -y ruby asciidoc xmlto
 
