40430e73d17a54c44056d0197d36d7c4b759af63
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2021-05-14 10:21:03 -0400

    jjb: lttng-tools: adjust post-build action to act on failure instead of abort
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- scripts/lttng-tools/tap_failure.sh
+++ scripts/lttng-tools/tap_failure.sh
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# This script is necessary since that for job failure due to timeout, the
+# gathering of tap log will not have been executed.
 set -exu
 
 # Required variables
