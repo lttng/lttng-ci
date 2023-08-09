09bc9215c416b482a0ef2f0d701e1d25a314c34b
Kienan Stewart <kstewart@efficios.com>
2023-08-09 11:52:54 -0400

    ansible: Add and manage replacement git-mirror instance
    Signed-off-by: Kienan Stewart <kstewart@efficios.com>
    Change-Id: Ib38ceeca6f83b05e975ad7013fcd6d2d52e793f3

---
+++ automation/ansible/roles/gitmirror/files/update.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# sudo -u gitdaemon git clone --mirror git://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git
+
+update_git() {
+    local repodir="$1"
+    local origin="$2"
+
+    if [ ! -d "${repodir}" ] ; then
+        git clone --bare "${origin}" "${repodir}"
+    fi
+
+    pushd "$repodir"
+
+    git remote update
+    #git gc
+    mkdir -p info/web
+    git for-each-ref --sort=-committerdate --format='%(committerdate:iso8601)' --count=1 >info/web/last-modified
+
+    popd
+}
+
+##
+# Vanilla composite repo
+##
+
+update_git linux-all.git/ https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+
+pushd linux-all.git/
+## Add stable if needed
+if ! git remote | grep -q stable ; then
+    git remote add stable https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
+    git fetch stable
+fi
+## Delete broken tree tags
+git tag -d v2.6.11 || true
+git tag -d v2.6.11-tree || true
+popd
+
+##
+# Ubuntu kernels
+##
+
+update_git ubuntu-xenial.git/ https://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/xenial
+
+update_git ubuntu-bionic.git/ git://kernel.ubuntu.com/ubuntu/ubuntu-bionic.git
+
+update_git ubuntu-focal.git/ git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/focal
+
+update_git ubuntu-jammy.git/ git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/jammy
+
+##
+# GDB repos
+##
+
+update_git binutils-gdb.git/ git://sourceware.org/git/binutils-gdb.git
