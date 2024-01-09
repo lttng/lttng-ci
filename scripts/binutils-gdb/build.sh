f8743131d1055dff62c315373c9623b23fc9a44a
Simon Marchi <simon.marchi@efficios.com>
2024-01-09 10:42:51 -0500

    jjb: binutils-gdb: don't run tests in parallel
    We see some tests failing, where GDB gets SIGKILLed, most likely some
    out of memory problems.  Don't run test in parallel for now, to try to
    avoid these.
    
    Change-Id: I6e3ed399dd9376d5ab0316f85b77eb4c0b35c9e1

--- scripts/binutils-gdb/build.sh
+++ scripts/binutils-gdb/build.sh
@@ -300,7 +300,7 @@ esac
 $MAKE -C gdb/testsuite site.exp
 # shellcheck disable=SC2016
 echo 'set gdb_test_timeout [expr 5 * $timeout]' >> gdb/testsuite/site.exp
-$MAKE -C gdb --keep-going check -j "$($NPROC)" RUNTESTFLAGS="$RUNTESTFLAGS" FORCE_PARALLEL="1" || true
+$MAKE -C gdb --keep-going check RUNTESTFLAGS="$RUNTESTFLAGS" || true
 
 # Copy the dejagnu test results for archiving before cleaning the build dir
 mkdir "${WORKSPACE}/results"
