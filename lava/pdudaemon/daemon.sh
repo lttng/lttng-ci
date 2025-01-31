1cbeb9eaab49662bc74f224a4d41f103d6416441
Kienan Stewart <kstewart@efficios.com>
2025-01-31 14:50:00 -0500

    lava: Remove pdudaemon
    ipmitool/ipmiutil can be used to power up, down, and reset the
    physical machines used with lava.
    
    Change-Id: If3c56563765d3f82b138a7d6a792f67774769ca0
    Signed-off-by: Kienan Stewart <kstewart@efficios.com>

--- lava/pdudaemon/daemon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WORK_DIR="$(dirname "$(readlink -f "$0")")"
-VENV="${WORK_DIR}/venv/bin/activate"
-
-source "${VENV}"
-
-pdudaemon --journal --dbfile="${WORK_DIR}/pdudaemon.db" --conf="${WORK_DIR}/pdudaemon.conf"
