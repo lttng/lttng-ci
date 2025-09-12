dd289c099530117cc036c26a20911faad0b7d80d
Kienan Stewart <kstewart@efficios.com>
2025-09-12 09:11:34 -0400

    jjb: Improve cleanup during lttng-ivc tests
    The lttng-ivc tests would often leave large (~3.5GiB) folders in the
    /tmp after runs.
    
    This causes nodes to become flaky as the memory is filled and can't be
    automatically paged out (no swap).
    
    Apply two strategies:
    
     * Switch the temporary directory to one in the workspace, as is done
     with other jobs.
     * Add the cleanup commands to a list that is executed in reverse
     order during exit.
    
    Change-Id: I8a27473d73f9e69d4f180bc398d9b37444edf6f3
    Signed-off-by: Kienan Stewart <kstewart@efficios.com>

--- scripts/lttng-ivc/build.sh
+++ scripts/lttng-ivc/build.sh
@@ -16,11 +16,34 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 set -xu
+cleanup_commands=()
 
+function cleanup {
+    for (( idx="${#cleanup_commands[@]}" - 1; idx >= 0 ; idx-- )); do
+        ${cleanup_commands[${idx}]}
+    done
+}
+
+trap cleanup EXIT
 PYTHON3="python3"
+
+# Create tmp directory
+TMPDIR="$WORKSPACE/tmp"
+mkdir -p "$TMPDIR"
+
+# Use a symlink in /tmp to point to the the tmp directory
+# inside the workspace, this is to work around the path length
+# limit of unix sockets which are created by the test suite.
+tmpdir="$(mktemp)"
+ln -sf "$TMPDIR" "$tmpdir"
+cleanup_commands+=("rm -f '${tmpdir}'")
+export TMPDIR="$tmpdir"
+
 
 # Tox does not support long path venv for whatever reason.
 PYENV_HOME=$(mktemp -d)
+cleanup_commands+=("deactivate")
+cleanup_commands+=("rm -rf '${PYENV_HOME}'")
 
 # Create virtualenv and install necessary packages
 virtualenv --system-site-packages -p $PYTHON3 "$PYENV_HOME"
@@ -35,6 +58,7 @@ pip install --quiet tox
 # Hack for path too long in venv wrapper shebang
 TOXWORKDIR=$(mktemp -d)
 export TOXWORKDIR
+cleanup_commands+=("rm -rf '${TOXWORKDIR}'")
 
 cd src/ || exit 1
 
